e4bb0a52518bb0ef6d031e2e5dfcc1a81f86f17e
Fajrin0397 <rinrinpondr@yahoo.com>
2026-05-04 11:18:25 +0900

    menambahkan session nama

--- app.py
+++ app.py
@@ -1,37 +1,102 @@
-
 from flask_socketio import SocketIO, emit
-#from flask_cors import CORS
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
-#CORS(app)
+app.secret_key = "secret123"
 
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# ======================
+# DATABASE SEDERHANA
+# ======================
+users_db = {
+    "rin": "123",
+    "andi": "123"
+}
+
+# ======================
+# USER ONLINE
+# ======================
+online_users = {}
+
+# ======================
+# LOGIN
+# ======================
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username  and users_db['rin'] == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            return "Login gagal"
+
+    return render_template('login.html')
+
+# ======================
+# MAIN PAGE
+# ======================
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' not in session:
+        return redirect('/login')
 
-clickMarkers = {}
-idLinks = {}
+    return render_template('index.html', username=session['username'])
 
+# ======================
+# SOCKET CONNECT
+# ======================
 @socketio.on('connect')
-def client_connect():
-    emit('on_connect', 'connected')
-    emit('update_marker', clickMarkers)
+def connect():
+    username = session.get('username')
 
-@socketio.on('on_click')
-def on_click(sendlist):
-    clickMarkers[sendlist[0]] = sendlist[1]
-    idLinks[request.sid]=sendlist[0]
-    emit('update_marker', clickMarkers, broadcast=True)
+    if not username:
+        return False  # tolak kalau belum login
 
+    sid = request.sid
+
+    online_users[sid] = {
+        "name": username,
+        "lat": None,
+        "lng": None
+    }
+
+    emit('init', {
+        "sid": sid,
+        "users": online_users
+    })
+
+    emit('user_joined', online_users[sid], broadcast=True)
+
+# ======================
+# UPDATE LOKASI
+# ======================
+@socketio.on('update_location')
+def update_location(data):
+    sid = request.sid
+
+    if sid in online_users:
+        online_users[sid]["lat"] = data["lat"]
+        online_users[sid]["lng"] = data["lng"]
+
+    emit('update_users', online_users, broadcast=True)
+
+# ======================
+# DISCONNECT
+# ======================
 @socketio.on('disconnect')
-def client_disconnect():
-    if request.sid in idLinks:
-        print('marker remove')
-        del clickMarkers[idLinks[request.sid]]
-        emit('remove_marker', idLinks[request.sid], broadcast=True)
+def disconnect():
+    sid = request.sid
 
+    if sid in online_users:
+        del online_users[sid]
+        emit('remove_user', sid, broadcast=True)
+
+# ======================
+# RUN
+# ======================
 if __name__ == "__main__":
-    print('server has run')
-    socketio.run(app,debug=True,)
+    socketio.run(app, debug=True)
